cb7c2346d10af1a7ae3291edac929121eee7b89f
CharlyCst <charly.castes@epfl.ch>
2020-02-13 10:30:45 +0100

    Cleaning up

--- test/test.go
+++ test/test.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"fmt"
-	"syscall"
-	"unsafe"
 
 	"test/mpk"
 )
 
-func add(x, y int64) int64
-
 func main() {
-	println(add(3, 4))
-	println(sysGetPID())
 	pkey, err := mpk.PkeyAlloc()
 	if err != nil {
 		fmt.Printf("Failed to allocate pkey, returned: %d\n", pkey)
@@ -28,7 +22,7 @@ func main() {
 	}
 
 	mpk.WritePKRU(1<<2 + 1<<3) // set execute only on key 1
-	fmt.Printf("Reading PKRU: %b\n", mpk.ReadPKRU())
+	fmt.Printf("Reading PKRU: Ob%032b\n", mpk.ReadPKRU())
 
 	err = mpk.PkeyFree(pkey)
 	if err != nil {
@@ -36,14 +30,4 @@ func main() {
 	} else {
 		fmt.Println("pkey has been deallocated")
 	}
-}
-
-// Warning: doesn't work
-func sysWrite(char uint) {
-	syscall.Syscall(4, 1, 1, (uintptr)(unsafe.Pointer(&char)))
-}
-
-func sysGetPID() uint {
-	pid, _, _ := syscall.Syscall(39, 0, 0, 0)
-	return (uint)(pid)
 }
