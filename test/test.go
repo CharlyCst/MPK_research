b1b40931c2327472a22a78a8dbe5ac7c356916c0
CharlyCst <charly.castes@epfl.ch>
2020-02-13 17:29:32 +0100

    Updating tests

--- test/test.go
+++ test/test.go
@@ -26,18 +26,20 @@ func main() {
 	x = 1
 	fmt.Println("Declaring var x with value:", x)
 
-	pkru := mpk.AllRightsPKRU
-	pkru = pkru.Update(pkey1, mpk.ProtX)
-	pkru = pkru.Update(pkey2, mpk.ProtRX)
-	mpk.WritePKRU(pkru)
-	fmt.Println("Reading PKRU:", mpk.ReadPKRU())
-
 	err = mpk.PkeyMprotect((uintptr(unsafe.Pointer(&x))>>12)<<12, 0x1000, pkey1)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	fmt.Println("Memory tagged with key 1")
 	x = 2
+
+	pkru := mpk.AllRightsPKRU
+	pkru = pkru.Update(pkey1, mpk.ProtX)
+	pkru = pkru.Update(pkey2, mpk.ProtRX)
+	mpk.WritePKRU(pkru)
+	fmt.Println("Reading PKRU:", mpk.ReadPKRU())
+
+	x = 3
 
 	err = mpk.PkeyFree(pkey2)
 	if err != nil {
