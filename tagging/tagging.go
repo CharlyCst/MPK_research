0b07652c4cb112417fa730b980ab588f4f9ff0d6
CharlyCst <charly.castes@epfl.ch>
2020-02-24 21:30:58 +0100

    Cleaning up

--- tagging/tagging.go
+++ tagging/tagging.go
@@ -11,16 +11,22 @@ import (
 type Sandbox struct {
 	Name          string
 	ID            int
-	Deps          map[int]*Pkg // Pkg.ID set
+	Deps          map[int]*Pkg // Complete dependencies
+	directDeps    map[int]*Pkg // Direct dependencies
 	depsToCluster []int
 }
 
 // NewSandbox returns a fresh sandbox
-func NewSandbox(name string, id int) *Sandbox {
+func NewSandbox(name string, id int, deps ...*Pkg) *Sandbox {
+	directDeps := make(map[int]*Pkg, len(deps))
+	for _, dep := range deps {
+		directDeps[dep.ID] = dep
+	}
 	return &Sandbox{
 		Name:          name,
 		ID:            id,
 		Deps:          make(map[int]*Pkg),
+		directDeps:    directDeps,
 		depsToCluster: make([]int, 0),
 	}
 }
@@ -66,17 +72,16 @@ func main() {
 	pkgRuntime := NewPkg("runtime", 1)
 	pkgSync := NewPkg("sync", 2)
 
-	sandboxA := NewSandbox("sb_A", 0)
-	sandboxB := NewSandbox("sb_B", 1)
-	sandboxC := NewSandbox("sb_C", 2)
+	sandboxA := NewSandbox("sb_A", 0, pkgIO)
+	sandboxB := NewSandbox("sb_B", 1, pkgRuntime)
+	sandboxC := NewSandbox("sb_C", 2, pkgSync)
 
 	pkgSet[0] = pkgIO
 	pkgSet[1] = pkgRuntime
 	pkgSet[2] = pkgSync
-	sandboxA.Deps[0] = pkgIO
-	sandboxB.Deps[1] = pkgRuntime
-	sandboxC.Deps[2] = pkgSync
-	// pkgIO.usedIn[0] = &sandboxA
+	// sandboxA.Deps[0] = pkgIO
+	// sandboxB.Deps[1] = pkgRuntime
+	// sandboxC.Deps[2] = pkgSync
 
 	crawlPackages("strings", pkgSet, sandboxA, sandboxB, sandboxC)
 
@@ -166,20 +171,17 @@ func popMap(m map[int][]int) (int, []int) {
 }
 
 func crawlPackages(rootPackage string, pkgSet map[int]*Pkg, sandboxes ...*Sandbox) {
-	pkgID := 0
+	pkgID := len(pkgSet)
 	pkgNameToID := make(map[string]int)
 	pkgQueue := make([]depth.Pkg, 0)
 	sbQueue := make([]int, 0)
 	activeSb := make([]*Sandbox, 0)
 
 	for id, pkg := range pkgSet {
-		if id <= pkgID {
-			pkgID = id + 1
-		}
-
 		pkgNameToID[pkg.Name] = id
 	}
 
+	// Use depth to resolve dependency tree
 	t := depth.Tree{
 		ResolveInternal: true,
 	}
@@ -188,58 +190,61 @@ func crawlPackages(rootPackage string, pkgSet map[int]*Pkg, sandboxes ...*Sandbo
 		log.Fatal(err)
 	}
 
-	for _, dep := range t.Root.Deps {
-		pkgQueue = append(pkgQueue, dep)
-		sbQueue = append(sbQueue, 0)
+	pkgQueue = append(pkgQueue, *t.Root)
+	sbQueue = append(sbQueue, 0)
+	for len(pkgQueue) > 0 {
+		lastIndex := len(pkgQueue) - 1
+		pkg := pkgQueue[lastIndex]
+		pkgQueue = pkgQueue[:lastIndex]
+		nbSb := sbQueue[lastIndex]
+		sbQueue = sbQueue[:lastIndex]
 
-		for len(pkgQueue) > 0 {
-			lastIndex := len(pkgQueue) - 1
-			pkg := pkgQueue[lastIndex]
-			pkgQueue = pkgQueue[:lastIndex]
-			nbSb := sbQueue[lastIndex]
-			sbQueue = sbQueue[:lastIndex]
+		// fmt.Println(pkg.Name)
+		// for _, sb := range activeSb {
+		// 	fmt.Print(sb.Name + " ")
+		// }
+		// fmt.Println()
 
-			// Register the package if never seen before
-			var pkgStruct *Pkg
-			id, exist := pkgNameToID[pkg.Name]
-			if !exist {
-				pkgStruct = NewPkg(pkg.Name, pkgID)
-				pkgSet[pkgID] = pkgStruct
-				pkgNameToID[pkg.Name] = pkgID
-				id = pkgID
-				pkgID++
-			} else {
-				pkgStruct, _ = pkgSet[id]
+		// Register the package if never seen before
+		var pkgStruct *Pkg
+		id, exist := pkgNameToID[pkg.Name]
+		if !exist {
+			pkgStruct = NewPkg(pkg.Name, pkgID)
+			pkgSet[pkgID] = pkgStruct
+			pkgNameToID[pkg.Name] = pkgID
+			id = pkgID
+			pkgID++
+		} else {
+			pkgStruct, _ = pkgSet[id]
+		}
+
+		// Add newly activated sandboxes
+		nbNewSb := 0
+		for _, sb := range sandboxes {
+			_, ok := sb.directDeps[id]
+			if ok {
+				nbNewSb++
+				activeSb = append(activeSb, sb)
 			}
+		}
 
-			// Add newly activated sandboxes
-			nbNewSb := 0
-			for _, sb := range sandboxes {
-				_, ok := sb.Deps[id]
-				if ok {
-					nbNewSb++
-					activeSb = append(activeSb, sb)
-				}
-			}
+		// Register the current package into all active sandboxes
+		for _, sb := range activeSb {
+			sb.Deps[id] = pkgStruct
+		}
 
-			// Register the current package into all active sandboxes
-			for _, sb := range activeSb {
-				sb.Deps[id] = pkgStruct
-			}
+		// Add all package dependencies to the queue
+		for _, pkgDep := range pkg.Deps {
+			pkgQueue = append(pkgQueue, pkgDep)
+		}
 
-			// Add all package dependencies to the queue
-			for _, pkgDep := range pkg.Deps {
-				pkgQueue = append(pkgQueue, pkgDep)
-			}
-
-			// Remove out of scope sandboxes
-			if len(pkg.Deps) == 0 {
-				activeSb = activeSb[:len(activeSb)-nbSb-nbNewSb]
-			} else {
-				sbQueue = append(sbQueue, nbSb+nbNewSb)
-				for i := 0; i < len(pkg.Deps)-1; i++ {
-					sbQueue = append(sbQueue, 0)
-				}
+		// Remove out of scope sandboxes
+		if len(pkg.Deps) == 0 {
+			activeSb = activeSb[:len(activeSb)-nbSb-nbNewSb]
+		} else {
+			sbQueue = append(sbQueue, nbSb+nbNewSb)
+			for i := 0; i < len(pkg.Deps)-1; i++ {
+				sbQueue = append(sbQueue, 0)
 			}
 		}
 	}
